355b7d513c64153b70e189ea8f443ed4a9c2d9fe
Joshua Moses <59664518+joshuanmoses@users.noreply.github.com>
2025-04-25 16:00:41 -0500

    Create utils.py

---
+++ core/utils.py
@@ -0,0 +1,6 @@
+import subprocess
+
+def run_command(cmd):
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    return stdout.decode(), stderr.decode(), process.returncode

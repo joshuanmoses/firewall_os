4f773d61407623b823baa05cc4c046d393ea5cf9
Joshua Moses <59664518+joshuanmoses@users.noreply.github.com>
2025-04-25 16:03:15 -0500

    Create vlan_service.py

---
+++ services/vlan_service.py
@@ -0,0 +1,10 @@
+import time
+from core.vlan_manager import setup_vlans
+from core.logger import setup_logger
+
+logger = setup_logger(__name__)
+
+def run():
+    while True:
+        setup_vlans()
+        time.sleep(300)  # Check VLANs every 5 minutes
